df2a465e2fde1e73de6a40bf4666738619021b01
ahmedkamal22 <ak0850000@gmail.com>
2024-07-13 19:46:38 +0300

    First commit in gazzer user app

---
+++ lib/features/notification/domain/service/notification_service_interface.dart
@@ -0,0 +1,13 @@
+import 'package:stackfood_multivendor/features/notification/domain/models/notification_model.dart';
+
+abstract class NotificationServiceInterface {
+  Future<List<NotificationModel>?> getList();
+
+  void saveSeenNotificationCount(int count);
+
+  int? getSeenNotificationCount();
+
+  List<int> getNotificationIdList();
+
+  void addSeenNotificationIdList(List<int> notificationList);
+}
---
+++ lib/features/onboard/domain/repository/onboard_repository_interface.dart
@@ -0,0 +1,3 @@
+import 'package:stackfood_multivendor/interface/repository_interface.dart';
+
+abstract class OnboardRepositoryInterface extends RepositoryInterface {}
